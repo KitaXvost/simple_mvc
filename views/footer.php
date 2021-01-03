d07f7a5e6d1a480cf0e1c8f089e2a367666813eb
KitaXvost <50918704+KitaXvost@users.noreply.github.com>
2021-01-03 22:14:09 +0300

    Update footer.php

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,7 @@
 </div>
 <footer style="margin:20px;">
 Copyright Andrei Puntus &copy; <?php echo date ( 'Y' ) ; ?></br>
-Code on <a href="https://github.com/KitaXvost/beejee">GitHub</a>
+Code on <a href="https://github.com/KitaXvost/simple_mvc">GitHub</a>
 </footer>
 </body>
 </html>
